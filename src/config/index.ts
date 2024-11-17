e2a10c8026e261e707c98bb415345d1e4f32efb6
imrannaazir <imrannaaziremon@gmail.com>
2024-11-17 15:58:48 +0600

    INIT: project boilerplate

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+const envPath =
+  process.env.NODE_ENV === 'production'
+    ? path.join(process.cwd(), '.env.prod')
+    : path.join(process.cwd(), '.env');
+
+dotenv.config({ path: envPath });
+
+export default {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
